7d5188f545decaa95daec3d4bdcd065e337f1acb
DavitEgoian <dato.egoyan@gmail.com>
2025-05-11 13:49:25 +0400

    Landing page has been developed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-import SplashCursor from './SplashCursor';
-import Magnet from './Magnet';
-import ShinyText from './ShinyText';
+import TextPressure from './TextPressure';
+import { useRef } from 'react';
+import VariableProximity from './VariableProximity';
+
 
 
 function App() {
+    const containerRef = useRef(null);
   return (
-    <div className="App">
-        <header className="App-header">
-          <Magnet padding={50} disabled={false} magnetStrength={50}>
-            <h1 >Davit Egoian</h1>
-            <p><ShinyText text="Coming soon" disabled={false} speed={3} className='custom-class' /></p>
-          </Magnet>
-        </header>
-        <SplashCursor />
-    </div>
+      <div className="App">
+          <div style={{position: 'static', height: 'auto', width:'100%', marginBottom: '1rem', display: 'block'}}>
+              <TextPressure
+                  text="Hello!"
+                  flex={true}
+                  alpha={false}
+                  stroke={false}
+                  width={true}
+                  weight={true}
+                  italic={true}
+                  textColor="#ffffff"
+                  strokeColor="#ff0000"
+                  minFontSize={36}
+              />
+              <br/>
+              <div
+            ref={containerRef}
+            style={{position: 'relative'}}
+            >
+              <VariableProximity
+                label={'I build intelligent systems that balance\n ' + 'accuracy,' +
+                    ' scalability and interpretability.'}
+                className={'variable-proximity-demo'}
+                fromFontVariationSettings="'wght' 400, 'opsz' 9"
+                toFontVariationSettings="'wght' 1000, 'opsz' 40"
+                containerRef={containerRef}
+                radius={100}
+                falloff='linear'
+              />
+              <a href='http://google.com' className='resetButtonHref'>
+                  <div className="resumeButton">
+                    <p>My Resume</p>
+                  </div>
+              </a>
+
+            </div>
+
+          </div>
+
+
+      </div>
   );
 }
 
