988e82ca6ee44d9a630748822b406e5e211d9648
DavitEgoian <dato.egoyan@gmail.com>
2025-05-27 20:27:36 +0400

    tech logo data add

---
+++ src/data/techLogos.js
@@ -0,0 +1,61 @@
+import gitLogo from "../images/tech-stack-logos/git-logo.png";
+import pythonLogo from "../images/tech-stack-logos/python-logo.png";
+import anacondaLogo from "../images/tech-stack-logos/anaconda-logo.png";
+import numpyLogo from "../images/tech-stack-logos/numPy-logo.png";
+import pandasLogo from "../images/tech-stack-logos/pandas-logo.png";
+import matplotlibLogo from "../images/tech-stack-logos/matplotlib-logo.png";
+import plotlyLogo from "../images/tech-stack-logos/plotly-logo.png";
+import scipySeekLogo from "../images/tech-stack-logos/scipy-seek-logo.png";
+import postmanLogo from "../images/tech-stack-logos/postman-logo.png";
+import flaskLogo from "../images/tech-stack-logos/flask-logo.png";
+import djangoLogo from "../images/tech-stack-logos/django-logo.png";
+import mySQLLogo from "../images/tech-stack-logos/mySQL-logo.png";
+import mongoDBLogo from "../images/tech-stack-logos/mongoDB-logo.png";
+import new4jLogo from "../images/tech-stack-logos/new4j-logo.png";
+import apacheSparkLogo from "../images/tech-stack-logos/apache-spark-logo.png";
+import dockerLogo from "../images/tech-stack-logos/docker-logo.png";
+import kubernetesLogo from "../images/tech-stack-logos/kubernetes-logo.png";
+import jenkinLogo from "../images/tech-stack-logos/jenkin-logo.png";
+import travisCILogo from "../images/tech-stack-logos/travisCI-logo.png";
+import circleCILogo from "../images/tech-stack-logos/circleCI-logo.png";
+import githubActionLogo from "../images/tech-stack-logos/github-actions-logo.png";
+import gitLabLogo from "../images/tech-stack-logos/gitlab-logo.png";
+import scikitLearnLogo from "../images/tech-stack-logos/scikit-learn-logo.png";
+import kerasLogo from "../images/tech-stack-logos/keras-logo.png";
+import pytorchLogo from "../images/tech-stack-logos/pyTorch-logo.png";
+import prometheusLogo from "../images/tech-stack-logos/prometheus-logo.png";
+import grafanaLogo from "../images/tech-stack-logos/grafana-logo.png";
+import powerBILogo from "../images/tech-stack-logos/power_BI_logo.png";
+
+const techLogos = [
+  { src: gitLogo, alt: "Git Logo" },
+  { src: pythonLogo, alt: "Python Logo" },
+  { src: anacondaLogo, alt: "Anaconda Logo" },
+  { src: numpyLogo, alt: "NumPy Logo" },
+  { src: pandasLogo, alt: "Pandas Logo" },
+  { src: matplotlibLogo, alt: "Matplotlib Logo" },
+  { src: plotlyLogo, alt: "Plotly Logo" },
+  { src: scipySeekLogo, alt: "SciPy Logo" },
+  { src: postmanLogo, alt: "Postman Logo" },
+  { src: flaskLogo, alt: "Flask Logo" },
+  { src: djangoLogo, alt: "Django Logo" },
+  { src: mySQLLogo, alt: "MySQL Logo" },
+  { src: mongoDBLogo, alt: "MongoDB Logo" },
+  { src: new4jLogo, alt: "Neo4j Logo" },
+  { src: apacheSparkLogo, alt: "Apache Spark Logo" },
+  { src: dockerLogo, alt: "Docker Logo" },
+  { src: kubernetesLogo, alt: "Kubernetes Logo" },
+  { src: jenkinLogo, alt: "Jenkins Logo" },
+  { src: travisCILogo, alt: "Travis CI Logo" },
+  { src: circleCILogo, alt: "CircleCI Logo" },
+  { src: githubActionLogo, alt: "GitHub Actions Logo" },
+  { src: gitLabLogo, alt: "GitLab CI/CD Logo" },
+  { src: scikitLearnLogo, alt: "Scikit-Learn Logo" },
+  { src: kerasLogo, alt: "Keras Logo" },
+  { src: pytorchLogo, alt: "PyTorch Logo" },
+  { src: prometheusLogo, alt: "Prometheus Logo" },
+  { src: grafanaLogo, alt: "Grafana Logo" },
+  { src: powerBILogo, alt: "Power BI Logo" }
+];
+
+export default techLogos;
