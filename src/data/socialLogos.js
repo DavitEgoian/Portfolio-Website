ad921c7697a9f378f9730931e548f857fa71d629
DavitEgoian <dato.egoyan@gmail.com>
2025-05-29 00:05:05 +0400

    social Logo mapping all of logos

--- src/data/socialLogos.js
+++ src/data/socialLogos.js
@@ -1,8 +1,12 @@
 import githubLogo from "../images/social-media-logos/github-logo.png";
-
+// import kagggleLogo from '../images/social-media-logos/ka'
+import linkedInLogo from '../images/social-media-logos/linkedin-logo.png'
 
 const techLogos = [
   { src: githubLogo, alt: "GitHub Logo" },
+  //{ scr: kaggleLogo, alt: "Kaggle Logo"}
+  { src: linkedInLogo, alt: 'LinkedIn Logo'}
+
 ];
 
 export default techLogos;
