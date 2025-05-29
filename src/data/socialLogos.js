38bc8c19473ea48c38210370fe3c38ccea67c3db
DavitEgoian <dato.egoyan@gmail.com>
2025-05-29 13:12:43 +0400

    href add

--- src/data/socialLogos.js
+++ src/data/socialLogos.js
@@ -3,9 +3,9 @@ import githubLogo from "../images/social-media-logos/github-logo.png";
 import linkedInLogo from '../images/social-media-logos/linkedin-logo.png'
 
 const techLogos = [
-  { src: githubLogo, alt: "GitHub Logo" },
+  { src: githubLogo, alt: "GitHub Logo", href: 'https://github.com/davitegoian' },
   //{ scr: kaggleLogo, alt: "Kaggle Logo"}
-  { src: linkedInLogo, alt: 'LinkedIn Logo'}
+  { src: linkedInLogo, alt: 'LinkedIn Logo', href: 'https://www.linkedin.com/in/davitegoian/'}
 
 ];
 
