de7c1f3b6d37d0dad2ad35234e5e879f8f7a973a
DavitEgoian <dato.egoyan@gmail.com>
2025-06-06 00:17:07 +0400

    Experience timeline highlight text add

--- src/data/experienceTimeline.js
+++ src/data/experienceTimeline.js
@@ -1,12 +1,14 @@
 const educationTimeline = [
   {
     title: "Python Internship",
-    desc: "UniLab",
+    highlighted: "UniLab",
+    desc: " — developing Python scripts for data processing, API integration, automation of workflows, and code optimization to support research projects.",
     date: "May 2025 - Present",
   },
   {
     title: "AI & Data Science Club Member",
-    desc: "Georgian AI Association",
+    highlighted: "Georgian AI Association",
+    desc: " — collaborating on AI/ML workshops, exploring data modeling and feature engineering, and participating in hackathons to apply data science techniques.",
     date: "Apr 2024 - Jun 2024",
   },
 ];
