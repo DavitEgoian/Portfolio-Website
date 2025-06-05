06fbca1b1008d927a2e15f972013dddee30d068a
DavitEgoian <dato.egoyan@gmail.com>
2025-06-05 14:18:05 +0400

    Education timeline content add

---
+++ src/data/educationTimeline.js
@@ -0,0 +1,19 @@
+const educationTimeline = [
+  {
+    title: "Georgian National University",
+    desc: "Bachelor of Computer Science, Data Science and Artificial Intelligence",
+    date: "Sep 2024 - Jul 2028",
+  },
+  {
+    title: "TBC Bank",
+    desc: "Web Development Fundamentals (Front End: HTML, CSS)",
+    date: "Apr 2024 - Jun 2024",
+  },
+  {
+    title: "IT Academy Step Georgia",
+    desc: "Software Development",
+    date: "Apr 2020 - Nov 2023",
+  },
+];
+
+export default educationTimeline;
