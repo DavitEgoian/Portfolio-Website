c3a1db4ab112e5de65b91d363260d34a862ba084
DavitEgoian <dato.egoyan@gmail.com>
2025-06-05 14:18:21 +0400

    experience timeline content add

---
+++ src/data/experienceTimeline.js
@@ -0,0 +1,14 @@
+const educationTimeline = [
+  {
+    title: "Python Internship",
+    desc: "UniLab",
+    date: "May 2025 - Present",
+  },
+  {
+    title: "AI & Data Science Club Member",
+    desc: "Georgian AI Association",
+    date: "Apr 2024 - Jun 2024",
+  },
+];
+
+export default educationTimeline;
