0ef7fabdf41f1924b8db2c9de9e137c217505e13
DavitEgoian <dato.egoyan@gmail.com>
2025-06-05 14:26:58 +0400

    Discord logo imported

--- src/data/socialLogos.js
+++ src/data/socialLogos.js
@@ -1,6 +1,7 @@
 import githubLogo from "../images/social-media-logos/github-logo.png";
 // import kagggleLogo from '../images/social-media-logos/ka'
 import linkedInLogo from "../images/social-media-logos/linkedin-logo.png";
+import discordLogo from "../images/social-media-logos/discord-logo.png";
 
 const techLogos = [
   {
@@ -13,6 +14,11 @@ const techLogos = [
     src: linkedInLogo,
     alt: "LinkedIn Logo",
     href: "https://www.linkedin.com/in/davitegoian/",
+  },
+  {
+    src: discordLogo,
+    alt: "Discord Logo",
+    href: "http://discordapp.com/users/705718800254763088",
   },
 ];
 
