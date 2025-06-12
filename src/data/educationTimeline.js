88de407abe0cde5f4fd55c67f716f8268c18908f
DavitEgoian <dato.egoyan@gmail.com>
2025-06-12 16:23:37 +0400

    Education timeline slight gramatical fix

--- src/data/educationTimeline.js
+++ src/data/educationTimeline.js
@@ -3,19 +3,19 @@ const educationTimeline = [
     title: "Georgian National University",
     highlighted:
       "Bachelor of Computer Science, Data Science and Artificial Intelligence",
-    desc: " - Algorithms, Probability Theory and Statistics, Artificial Neural Networks, and Foundations of Artificial Intelligence for scalable intelligent systems.",
+    desc: " - Algorithms, Probability Theory and Statistics, Artificial Neural Networks, and Foundations of Artificial Intelligence for Scalable Intelligent Systems.",
     date: "Sep 2024 - Jul 2028",
   },
   {
     title: "TBC Bank",
     highlighted: "Web Development Fundamentals",
-    desc: " — Semantic HTML5, responsive CSS and Sass with Flexbox/Grid, cross-browser compatibility, and Bootstrap.",
+    desc: " — Semantic HTML5, responsive CSS and Sass with Flexbox and Grid, cross-browser compatibility, and Bootstrap.",
     date: "Apr 2024 - Jun 2024",
   },
   {
     title: "IT Academy Step Georgia",
     highlighted: "Software Development",
-    desc: " — ASP, MS SQL Server Programming and Administration, Microsoft .NET Framework and C# Programming Language, JavaScript Programming Language and jQuery, Creating Web-pages using HTML5 and CSS3.",
+    desc: " — ASP, MS SQL Server Programming and Administration, Microsoft .NET Framework and C# Programming Language, JavaScript Programming Language and jQuery, creating web pages using HTML5 and CSS3.",
     date: "Apr 2020 - Nov 2023",
   },
 ];
