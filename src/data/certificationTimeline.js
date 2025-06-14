a592ce70a954bb9eca09846c28f4f50e5ba11e37
DavitEgoian <dato.egoyan@gmail.com>
2025-06-14 20:13:55 +0400

    Certification timeline data add

---
+++ src/data/certificationTimeline.js
@@ -0,0 +1,34 @@
+const certificationTimeline = [
+  {
+    name: "AI Engineer for Data Scientists Associate",
+    issueOrganization: "DataCamp",
+    desc: " - Developing Python pipelines for data management, exploratory data analysis, model development, ethical AI compliance, and data‑science programming, integrating REST APIs, Docker, Kubernetes, and CI/CD.",
+    issueDate: "Jun 2025",
+  },
+  {
+    name: "GitHub Foundations",
+    issueOrganization: "GitHub",
+    desc: " - Mastering Git and GitHub fundamentals, repository workflows, collaboration features, modern development practices, project management, privacy and security administration, and GitHub community benefits.",
+    issueDate: "Jun 2025",
+  },
+  {
+    name: "100 Days of Code: The Complete Python Pro Bootcamp",
+    issueOrganization: "Udemy",
+    desc: " - Building Flask web apps, integrating AI in finance, analyzing data with NumPy/Pandas, creating Plotly visualizations, managing PostgreSQL, and deploying Scikit-Learn models.",
+    issueDate: "Apr 2025",
+  },
+  {
+    name: "AWS Knowledge: Serverless",
+    issueOrganization: "Amazon",
+    desc: " - Designing and deploying serverless applications on AWS—leveraging API Gateway, Lambda, and other cloud-native services.",
+    issueDate: "Sep 2024",
+  },
+  {
+    name: "IT Specialist - Software Development",
+    issueOrganization: "Certiport",
+    desc: " - Applications—from desktop to web—are built with core programming and object-oriented design, supported by scalable and reliable database systems.",
+    issueDate: "Apr 2023",
+  },
+];
+
+export default certificationTimeline;
