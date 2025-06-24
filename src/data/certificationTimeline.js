8f0efec51d64bafe1139f44dd4dc909121ad915c
DavitEgoian <dato.egoyan@gmail.com>
2025-06-24 11:26:17 +0400

    Certification timeline links add

--- src/data/certificationTimeline.js
+++ src/data/certificationTimeline.js
@@ -1,33 +1,40 @@
+import { link } from "framer-motion/client";
+
 const certificationTimeline = [
   {
     name: "AI Engineer for Data Scientists Associate",
     issueOrganization: "DataCamp",
     desc: " - Developing Python pipelines for data management, exploratory data analysis, model development, ethical AI compliance, and data‑science programming, integrating REST APIs, Docker, Kubernetes, and CI/CD.",
     issueDate: "Jun 2025",
+    link: "https://www.datacamp.com/certificate/AEDS0017038047645",
   },
   {
     name: "GitHub Foundations",
     issueOrganization: "GitHub",
     desc: " - Mastering Git and GitHub fundamentals, repository workflows, collaboration features, modern development practices, project management, privacy and security administration, and GitHub community benefits.",
     issueDate: "Jun 2025",
+    link: "https://www.credly.com/badges/ccf37b2b-bc27-4a36-b85e-871844c64211/",
   },
   {
     name: "100 Days of Code: The Complete Python Pro Bootcamp",
     issueOrganization: "Udemy",
     desc: " - Building Flask web apps, integrating AI in finance, analyzing data with NumPy/Pandas, creating Plotly visualizations, managing PostgreSQL, and deploying Scikit-Learn models.",
     issueDate: "Apr 2025",
+    link: "https://www.udemy.com/certificate/UC-28c5b193-13af-4102-b7f9-7bfc6208a17e/",
   },
   {
     name: "AWS Knowledge: Serverless",
     issueOrganization: "Amazon",
     desc: " - Designing and deploying serverless applications on AWS—leveraging API Gateway, Lambda, and other cloud-native services.",
     issueDate: "Sep 2024",
+    link: "https://www.credly.com/badges/867bb136-e055-4ea8-a27c-786f51007a55/",
   },
   {
     name: "IT Specialist - Software Development",
     issueOrganization: "Certiport",
     desc: " - Applications—from desktop to web—are built with core programming and object-oriented design, supported by scalable and reliable database systems.",
     issueDate: "Apr 2023",
+    link: "https://www.credly.com/badges/42993002-c59e-400f-8bcd-716c1b43dc74/",
   },
 ];
 
